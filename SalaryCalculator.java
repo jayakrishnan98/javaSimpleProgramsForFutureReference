b26bbd4b73c38159b5132d1254910199d865b2f0
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2020-12-28 19:34:16 +0530

    fourth program completed

---
+++ SalaryCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SalaryCalculator {
+    public static void main(String[] ar) {
+        Scanner sc = new Scanner(System.in);
+        double salary;
+        double sales;
+        double basic = 4000;
+        double conveyance = 500;
+        double hra;
+        double da = 1.1 * basic;
+        double incentive;
+        double bonus;
+        System.out.print("Enter the sales;");
+        sales = sc.nextFloat();
+
+        if (sales >= 100000) {
+            hra = 0.2 * basic;
+            incentive = 0.1 * sales;
+            bonus = 1000;
+        } else {
+            hra = 0.1 * basic;
+            incentive = 0.04 * sales;
+            bonus = 5000;
+        }
+        salary = basic + hra + da + conveyance + incentive + bonus;
+        System.out.println("Salary of Sales executive =" + salary);
+    }
+}
+
